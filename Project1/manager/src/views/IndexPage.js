f7e325816ab399edfb59eb81b520d9143182d7dd
zhengqiang <1500651289@qq.com>
2019-06-11 21:29:13 +0800

    完善登录功能

--- Project1/manager/src/views/IndexPage.js
+++ Project1/manager/src/views/IndexPage.js
@@ -7,10 +7,10 @@ function IndexPage(props){
   // 获取login
   let {login} = props;
   useEffect(()=>{
-    // login({
-    //   user_name: 'chenmanjie',
-    //   user_pwd: 'Chenmanjie123!'
-    // })
+    login({
+      user_name: 'chenmanjie',
+      user_pwd: 'Chenmanjie123!'
+    })
   }, []);
 
   // 处理表单提交
@@ -29,42 +29,50 @@ function IndexPage(props){
   };
   // 表单校验
   const { getFieldDecorator } = props.form;
-  return  <Form onSubmit={handleSubmit} className="login-form">
-    <Form.Item>
-      {getFieldDecorator('username', {
-        validateTrigger: 'onBlur',
-        rules: [{required: true, message: '请输入正确的用户名'}],
-      })(
-        <Input
-          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          placeholder="请输入用户名"
-        />,
-      )}
-    </Form.Item>
-    <Form.Item>
-      {getFieldDecorator('password', {
-        rules: [{ pattern: /^(?:(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])).*$/, message: '请输入正确的密码' }],
-      })(
-        <Input
-          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-          type="password"
-          placeholder="请输入密码"
-        />,
-      )}
-    </Form.Item>
-    <Form.Item>
-      {getFieldDecorator('remember', {
-        valuePropName: 'checked',
-        initialValue: true,
-      })(<Checkbox>记住密码</Checkbox>)}
-      <a className="login-form-forgot" href="">
-        忘记密码
-      </a>
-      <Button type="primary" htmlType="submit" className="login-form-button">
-        登录
-      </Button>
-    </Form.Item>
-  </Form>;
+  return (
+    <div className={styles.box}>
+      <div className={styles.nav}>
+        <div className={styles.navs}>
+          <Form onSubmit={handleSubmit} className="login-form">
+            <Form.Item>
+              {getFieldDecorator('username', {
+                validateTrigger: 'onBlur',
+                rules: [{required: true, message: '请输入正确的用户名'}],
+              })(
+                <Input
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  placeholder="请输入用户名"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ pattern: /^(?:(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])).*$/, message: '请输入正确的密码' }],
+              })(
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="请输入密码"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(<Checkbox>记住密码</Checkbox>)}
+              <a className="login-form-forgot" href="">
+                忘记密码
+              </a>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 // props的类型检查
